9240a4fb3ff123dd491da2e1a5d4533b15e17f0e
aniasobo <sobolewska.a.m@gmail.com>
2021-06-05 12:13:48 +0100

    first spec passing

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,1 +1,12 @@
-# code
+module Parser
+  module Cron_expression
+    FIELDS = ['minute', 'hour', 'day of month', 'month', 'day of week', 'command']
+
+    def self.parse(input)
+    end
+
+    def self.generate_table
+      FIELDS.map { |field| field.ljust(14) + 'data'}
+    end
+  end
+end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,21 @@
 require 'parser'
 require 'pry'
 
-RSpec.describe Parser do
+RSpec.describe Parser::Cron_expression do
+  describe 'Parser::Cron_expression#parse' do
+  end
+
+  describe 'Parser::Cron_expression#generate_table' do
+    it 'prints the columns with correct spacing' do
+      body = described_class.generate_table
+
+      # puts body
+      expect(body).to eq(["minute        data",
+                          "hour          data",
+                          "day of month  data",
+                          "month         data",
+                          "day of week   data",
+                          "command       data"])
+    end
+  end
 end
